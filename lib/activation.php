0b987c810ffc764d75c616a59132b3dfda6364a1
Aristeides Stathopoulos <aristath@gmail.com>
2013-05-08 14:50:00 +0300

    Assets redirection on multisite https://github.com/retlehs/roots/commit/fba4b0cc5b041bcb7399c6f5b19c25712850da88

--- lib/activation.php
+++ lib/activation.php
@@ -190,7 +190,11 @@ function shoestrap_theme_activation_action() {
     $shoestrap_theme_activation_options['change_uploads_folder'] = false;
 
     update_option('uploads_use_yearmonth_folders', 0);
-    update_option('upload_path', 'assets');
+    if (!is_multisite()) {
+      update_option('upload_path', 'assets');
+    } else {
+      update_option('upload_path', '');
+    }
   }
 
   if ($shoestrap_theme_activation_options['create_navigation_menus'] === 'true') {
