49c805fb3d1063dcd953bb65362845ae334da484
Aristeides Stathopoulos <aristath@gmail.com>
2013-05-08 17:58:44 +0300

    small improvement for the search-form style

--- templates/searchform.php
+++ templates/searchform.php
@@ -1,5 +1,5 @@
 <?php do_action( 'shoestrap_pre_searchform' ); ?>
-<form role="search" method="get" id="searchform" class="form-search row" action="<?php echo home_url( '/' ); ?>">
+<form role="search" method="get" id="searchform" class="form-search row" action="<?php echo home_url( '/' ); ?>" style="margin-left: 0;">
   <label class="hide" for="s"><?php _e( 'Search for:', 'shoestrap' ); ?></label>
   <input type="text" value="<?php if ( is_search() ) echo get_search_query(); ?>" name="s" id="s" class="search-query col col-lg-8" placeholder="<?php _e( 'Search', 'shoestrap' ); ?> <?php bloginfo( 'name' ); ?>">
   <input type="submit" id="searchsubmit" value="<?php _e('Search', 'shoestrap'); ?>" class="btn col col-lg-4">
