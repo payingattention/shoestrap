d71facf6870072ff0bff3c1e5a170965e6af7f36
Aristeides Stathopoulos <aristath@gmail.com>
2013-05-12 17:31:32 +0300

    Better login screen
    Applying customizer settings, caching the resulting css and minimizing
    the css on output

--- lib/customizer/functions/login.php
+++ lib/customizer/functions/login.php
@@ -3,40 +3,33 @@
 Description: Customize WordPress Login Screen with options made in customizer.
 */
 function shoestrap_login_scripts() { 
-  if ( get_theme_mod( 'shoestrap_logo' ) ) {
-    $login_logo = get_theme_mod( 'shoestrap_logo' );
-  }
-  if ( get_theme_mod( 'shoestrap_background_color' ) ) {
-    $background_color = get_theme_mod( 'shoestrap_background_color' );
-  }
-  if ( get_theme_mod( 'shoestrap_header_backgroundcolor' ) ) {
-    $header_bg_color = get_theme_mod( 'shoestrap_header_backgroundcolor' );
-  }
-  if ( get_theme_mod( 'shoestrap_header_textcolor' ) ) {
-    $header_sitename_color = get_theme_mod( 'shoestrap_header_textcolor' );
-  }
-  if ( get_theme_mod( 'shoestrap_buttons_color' ) ) {
-    $btn_color = get_theme_mod( 'shoestrap_buttons_color' );
-  }
-  if ( get_theme_mod( 'shoestrap_link_color' ) ) {
-    $link_color = get_theme_mod( 'shoestrap_link_color' );
-  }
-  if ( get_theme_mod( 'shoestrap_google_webfonts' ) ) {
-    $google_webfonts = get_theme_mod( 'shoestrap_google_webfonts' );
-  }
+  if ( get_theme_mod( 'shoestrap_logo' ) )              $login_logo = get_theme_mod( 'shoestrap_logo' );
+  if ( get_theme_mod( 'shoestrap_background_color' ) )  $background_color = get_theme_mod( 'shoestrap_background_color', '#FFFFFF' );
+  if ( get_theme_mod( 'shoestrap_text_color' ) )        $textcolor = get_theme_mod( 'shoestrap_text_color', '#333333' );
+  if ( get_theme_mod( 'shoestrap_buttons_color' ) )     $btn_color = get_theme_mod( 'shoestrap_buttons_color', '#0066CC' );
+  if ( get_theme_mod( 'shoestrap_link_color' ) )        $link_color = get_theme_mod( 'shoestrap_link_color', '#0088CC' );
+  if ( get_theme_mod( 'shoestrap_google_webfonts' ) )   $google_webfonts = get_theme_mod( 'shoestrap_google_webfonts' );
+
 
   // Make sure colors are properly formatted
-  $btn_color = '#' . str_replace( '#', '', $btn_color );
-  // if no color has been selected, set to #0066cc. This prevents errors with the php-less compiler.
-  if ( strlen( $btn_color ) < 3 ) {
-    $btn_color = '#0066cc';
-  }
-  if ( get_theme_mod( 'shoestrap_flat_buttons' ) == 1 ) {
+  $btn_color              = '#' . str_replace( '#', '', $btn_color );
+  $background_color       = '#' . str_replace( '#', '', $background_color );
+  $textcolor              = '#' . str_replace( '#', '', $textcolor );
+  $link_color             = '#' . str_replace( '#', '', $link_color );
+
+  // Flat buttons handling
+  if ( get_theme_mod( 'shoestrap_flat_buttons' ) == 1 || get_theme_mod( 'shoestrap_general_no_gradients' ) == 1 ) {
     $btnColorHighlight = $btn_color;
   } else {
-    $btnColorHighlight = shoestrap_adjust_brightness( $btn_color, -63 );
+    // calcutate button colors
+    if ( shoestrap_get_brightness( $btn_color ) <= 60) {
+      $btnColorHighlight = shoestrap_adjust_brightness( $btn_color, 63 );
+    } else {
+      $btnColorHighlight = shoestrap_adjust_brightness( $btn_color, -63 );
+    }
   }
 
+  // Button text color depending on the button background color
   if ( shoestrap_get_brightness( $btn_color ) <= 160) {
     $textColor = '#ffffff';
   } else {
@@ -44,71 +37,74 @@ function shoestrap_login_scripts() {
   }
   $startColor = $btn_color;
   $endColor   = $btnColorHighlight;
-?>
-  <style type="text/css">
-    body.login {
-      background: <?php echo $background_color ?>;
-      background-size: contain;
-      font-family: <?php echo $google_webfonts ?>;
-    }
-    body.login div#login h1 a {
-      background-image: url(<?php echo $login_logo ?>);
-      background: <?php echo $header_bg_color ?>;
-      padding-bottom: 30px;
-      margin: 0 auto;
-    }
-    .login form {
-      background: <?php echo $background_color ?>;
-      border: 0px;
-      border-width: 0px;
-      border-style: none;
-      -ms-box-shadow: none;
-      -webkit-box-shadow: none;
-      -o-box-shadow: none;
-      -moz-box-shadow: none;
-      box-shadow: none;
-      -ms-border-radius: 0px;
-      -webkit-border-radius: 0px;
-      -o-border-radius: 0px;
-      -moz-border-radius: 0px;
-      border-radius: 0px;
-    }
-    #nav, #backtoblog {
-      position: relative;
-      left: 0px;
-    }
-    .login .message {
-      background-color: <?php echo $background_color ?>;
-      -ms-box-shadow: none;
-      -webkit-box-shadow: none;
-      -o-box-shadow: none;
-      -moz-box-shadow: none;
-      box-shadow: none;
-    }
-    .login #nav a, .login #backtoblog a, a, a.active, a:hover, a.hover, a.visited, a:visited, a.link, a:link {
-      color: <?php echo $link_color ?>;
-      text-decoration: none;
-    }
-    #wp-submit.button-primary {
-      color: <?php echo $textColor ?>;
-      background-color: <?php shoestrap_mix_colors( $startColor, $endColor, 60 ) ?>;
-      background-image: -moz-linear-gradient(top, <?php echo $startColor ?>, <?php echo $endColor ?> );
-      background-image: -webkit-gradient(linear, 0 0, 0 100%, from(<?php echo $startColor ?>), to(<?php echo $endColor ?>));
-      background-image: -webkit-linear-gradient(top, <?php echo $startColor ?>, <?php echo $endColor ?> );
-      background-image: -o-linear-gradient(top, <?php echo $startColor ?>, <?php echo $endColor ?> );
-      background-image: linear-gradient(to bottom, <?php echo $startColor ?>, <?php echo $endColor ?> );
-      background-repeat: repeat-x;';
-      *background-color: <?php echo $endColor ?>;
-      }
-    .btn:hover, .btn-primary:hover, .btn-primary:active, .btn::active, .btn-primary.active .btn.active, .btn-primary.disabled, .btn.disabled, .btn-primary[disabled] .btn[disabled] {
-      color: <?php echo $textColor ?>;
-      background-color: <?php echo $endColor ?>;
-      *background-color: <?php shoestrap_adjust_brightness( $endColor, -12 ) ?>;
-    }
-    </style>  
-<?php 
+
+  // the styles that will be applied to the login screen
+  $styles = '';
+  $styles .= '<style type="text/css">';
+
+  $styles .= 'body.login {';
+  $styles .= 'background: ' . $background_color . ';';
+  $styles .= 'background-size: contain;';
+  $styles .= 'font-family: ' . $google_webfonts . ';';
+  $styles .= '}';
+
+  $styles .= 'body.login div#login h1 a {';
+  $styles .= 'background-image: url(' . $login_logo . ');';
+  $styles .= 'background-color: none;';
+  $styles .= 'background-color: transparent;';
+  $styles .= 'background-size: contain !important;';
+  $styles .= 'padding-bottom: 30px;';
+  $styles .= 'margin: 0 auto;';
+  $styles .= 'width: 100% !important;';
+  $styles .= '}';
+
+  $styles .= 'body.login div#login form#loginform, .login label, .login .message {';
+  if ( shoestrap_get_brightness( $background_color ) >= 160 ) $styles .= 'background:' . shoestrap_adjust_brightness( $background_color, -15 ) . ';';
+  else $styles .= 'background:' . shoestrap_adjust_brightness( $background_color, 15 ) . ';';
+  $styles .= 'color:' . $textcolor . ';';
+  $styles .= 'border: 0px;';
+  $styles .= 'border-width: 0px;';
+  $styles .= 'border-style: none;';
+  $styles .= '-ms-box-shadow: none;';
+  $styles .= '-webkit-box-shadow: none;';
+  $styles .= '-o-box-shadow: none;';
+  $styles .= '-moz-box-shadow: none;';
+  $styles .= 'box-shadow: none;';
+  $styles .= '-ms-border-radius: 0px;';
+  $styles .= '-webkit-border-radius: 0px;';
+  $styles .= '-o-border-radius: 0px;';
+  $styles .= '-moz-border-radius: 0px;';
+  $styles .= 'border-radius: 0px;';
+  $styles .= '}';
+
+  $styles .= '.login #nav a, .login #backtoblog a, a, a.active, a:hover, a.hover, a.visited, a:visited, a.link, a:link {';
+  $styles .= 'color:' . $link_color . ';';
+  $styles .= 'text-decoration: none;';
+  $styles .= '}';
+
+  $styles .= '#wp-submit.button-primary {';
+  $styles .= 'color:' . $textColor . ';';
+  $styles .= 'background-color:' . shoestrap_mix_colors( $startColor, $endColor, 60 ) . ';';
+  $styles .= 'background-image:-moz-linear-gradient(top, ' . $startColor . ', ' . $endColor . ');';
+  $styles .= 'background-image: -webkit-gradient(linear, 0 0, 0 100%, from(' . $startColor . '), to(' . $endColor . '));';
+  $styles .= 'background-image: -webkit-linear-gradient(top, ' . $startColor . ', ' . $endColor . ');';
+  $styles .= 'background-image: -o-linear-gradient(top, ' . $startColor . ', ' . $endColor . ');';
+  $styles .= 'background-image: linear-gradient(to bottom, ' . $startColor . ', ' . $endColor . ');';
+  $styles .= 'background-repeat: repeat-x;';
+  $styles .= '*background-color:' . $endColor . ';';
+  $styles .= '}';
+
+  $styles .= '#wp-submit.button-primary:hover, #wp-submit.button-primary:active {';
+  $styles .= 'color:' . $textColor . ';';
+  $styles .= 'background-color:' . $endColor . ';';
+  $styles .= '*background-color:' . shoestrap_adjust_brightness( $endColor, -12 ) . ';';
+  $styles .= '}';
+
+  $styles .= '</style>';
+
+  return $styles;
+
 }
-add_action( 'login_enqueue_scripts', 'shoestrap_login_scripts' );
 
 /* Login Links */
 function shoestrap_login_logo_url() {
