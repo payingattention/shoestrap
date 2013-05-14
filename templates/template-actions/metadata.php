4bc3158b2ff64dabf6a926850c29339f48dd75de
Aristeides Stathopoulos <aristath@gmail.com>
2013-05-14 15:21:57 +0300

    better tags formatting on metadata

--- templates/template-actions/metadata.php
+++ templates/template-actions/metadata.php
@@ -3,26 +3,34 @@
 /*
  * The metadata template
  */
-function shoestrap_article_metadata() { ?>
+function shoestrap_article_metadata() {
+  // calculate how many columns there are for the current post's metadata
+  if ( has_tag() && get_comments_number() >= 1 )
+    $columnclass = 3;
+  elseif ( ( has_tag() && get_comments_number() < 1 ) || ( !has_tag() && get_comments_number() >= 1 ) )
+    $columnclass = 4;
+  elseif ( !has_tag() && get_comments_number() < 1 )
+    $columnclass = 6;
+  ?>
   <div class="row">
-    <div class="col col-lg-3">
+    <div class="col col-lg-<?php echo $columnclass; ?>">
       <div class="byline author vcard">
         <i class="glyphicon glyphicon-user"></i>
         <?php echo __( 'By', 'shoestrap' ); ?>
         <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a>
       </div>
     </div>
-    <div class="col col-lg-3">
+    <div class="col col-lg-<?php echo $columnclass; ?>">
       <i class="glyphicon time-icon glyphicon-time"></i>
       <time class="updated" datetime="<?php echo get_the_time( 'c' ); ?>" pubdate><?php echo get_the_date(); ?></time>
     </div>
-    <div class="col col-lg-3">
-      <?php if ( has_tag() ) { ?>
+    <?php if ( has_tag() ) { ?>
+      <div class="col col-lg-<?php echo $columnclass; ?>">
         <i class="glyphicon glyphicon-tags"></i>
-        <?php the_tags(''); ?>
-      <?php } ?>
-    </div>
-    <div class="col col-lg-3">
+        <?php the_tags('<span class="label label-tag">', '</span> <span class="label label-tag">', '</span>'); ?>
+      </div>
+    <?php } ?>
+    <div class="col col-lg-<?php echo $columnclass; ?>">
       <?php if ( get_comments_number() >= 1 ) { ?>
         <i class="glyphicon glyphicon-comment"></i>
         <?php comments_number(); ?>
