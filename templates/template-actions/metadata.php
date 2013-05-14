368ead46577aa12c2d5a9bf95d4df996f3c5c034
Aristeides Stathopoulos <aristath@gmail.com>
2013-05-14 16:21:22 +0300

    fixing metatags for screensizes < 768px and added conditional

--- templates/template-actions/metadata.php
+++ templates/template-actions/metadata.php
@@ -13,6 +13,7 @@ function shoestrap_article_metadata() {
     $columnclass = 6;
   ?>
   <div class="row meta-row">
+
     <div class="col col-lg-<?php echo $columnclass; ?>">
       <div class="byline author vcard">
         <i class="glyphicon glyphicon-user"></i>
@@ -20,24 +21,28 @@ function shoestrap_article_metadata() {
         <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a>
       </div>
     </div>
+
     <div class="col col-lg-<?php echo $columnclass; ?>">
       <i class="glyphicon time-icon glyphicon-time"></i>
       <time class="updated" datetime="<?php echo get_the_time( 'c' ); ?>" pubdate><?php echo get_the_date(); ?></time>
     </div>
-    <?php if ( has_tag() ) { ?>
-      <div class="col col-lg-<?php echo $columnclass; ?>">
-        <div class="tags-container">
-          <i class="glyphicon glyphicon-tags"></i>
-          <?php the_tags('<span class="label label-tag">', '</span> <span class="label label-tag">', '</span>'); ?>
-        </div>
-      </div>
-    <?php } ?>
+
     <?php if ( get_comments_number() >= 1 ) : ?>
       <div class="col col-lg-<?php echo $columnclass; ?>">
         <i class="glyphicon glyphicon-comment"></i>
         <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a>
       </div>
     <?php endif; ?>
+
+    <?php if ( has_tag() ) : ?>
+      <div class="col col-lg-<?php echo $columnclass; ?>">
+        <div class="tags-container">
+          <i class="glyphicon glyphicon-tags"></i>
+          <?php the_tags('<span class="label label-tag">', '</span> <span class="label label-tag">', '</span>'); ?>
+        </div>
+      </div>
+    <?php endif; ?>
+
   </div>
   <?php
 }
